c1e8d44a8caab2e8a0b45ceb178480d84b478004
Immaannn2222 <imennaayari@gmail.com>
2020-12-02 22:14:43 +0100

    check this task

--- math/0x00-linear_algebra/9-let_the_butcher_slice_it.py
+++ math/0x00-linear_algebra/9-let_the_butcher_slice_it.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""  Let The Butcher Slice It """
 matrix = np.array([[1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12],
                    [13, 14, 15, 16, 17, 18], [19, 20, 21, 22, 23, 24]])
 mat1 = matrix[1:3, :]
