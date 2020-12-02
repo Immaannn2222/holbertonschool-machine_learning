6c2a8b330b6511ed2bba0d5db892232173c87044
Immaannn2222 <imennaayari@gmail.com>
2020-12-02 23:54:14 +0100

    fix

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -5,6 +5,9 @@
 def add_matrices2D(mat1, mat2):
     """add two array element wise"""
     sum_l = []
-    for (x, y) in zip(mat1, mat2):
-        sum_l.append(x + y)
+    if len(mat1) is not len(mat2) or len(mat1[0]) is not len(mat2[0]):
+        return None
+    for i in range(len(mat1)):
+        for x, y in zip(mat1[i], mat2[i]):
+            sum_l.append(x + y)
     return sum_l
