a143a6da72b4b8a7e19303ec67cbd97691a2d26e
Immaannn2222 <imennaayari@gmail.com>
2020-12-03 15:45:28 +0100

    fract

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-""" first"""
+""" first """
 
 
 def add_matrices2D(mat1, mat2):
     """add two array element wise"""
     sum_l = []
-    if len(mat1) is not len(mat2) or len(mat1[0]) is not len(mat2[0]):
+    if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
     for i in range(len(mat1)):
         for x, y in zip(mat1[i], mat2[i]):
