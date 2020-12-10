d2fa38f1d9f29512031e1999db8b0775870ce290
Immaannn2222 <imennaayari@gmail.com>
2020-12-10 21:59:10 +0100

    fix

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+""" task 17"""
+
+
+def poly_integral(poly, C=0):
+    """ calculates the integral of a polynomial"""
+    intg_list = []
+    if not isinstance(poly, list) or poly == []:
+        return None
+    if not isinstance(C, (int, float)):
+        return None
+    intg_list.append(C)
+    for i in range(len(poly)):
+        intg_list.append(poly[i] / (i + 1))
+    return intg_list
