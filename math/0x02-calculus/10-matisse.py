0df8d51b039eb9fd6d371a20e656816aaf510318
Immaannn2222 <imennaayari@gmail.com>
2020-12-10 20:48:19 +0100

    fix

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""task 10"""
+""" task 10 """
 
 
 def poly_derivative(poly):
-    """calculates the derivative of a polynomial"""
+    """ calculates the derivative of a polynomial """
     if poly == [0] or len(poly) < 1:
         return [0]
     if not isinstance(enumerate(poly[:1], 0), (int, float)) or not isinstance(poly, list) or poly == []:
--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""9th task"""
+""" 9th task """
 
 
 def summation_i_squared(n): 
-    """calculates sum of i^2"""
-    if not isinstance (n, (int, float)) or n < 0:
+    """ calculates sum of i^2 """
+    if not isinstance (n, (int, float)) or n < 1:
         return None
     return  (n * (n + 1) * (2 * n + 1)) // 6
