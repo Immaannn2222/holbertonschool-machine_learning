95bde558ed65bd0e7bd9ccd3cfe3ea36b086c727
Immaannn2222 <imennaayari@gmail.com>
2020-12-10 22:44:14 +0100

    fix

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -11,10 +11,12 @@ def poly_integral(poly, C=0):
     if not isinstance(poly, list) or not all(isinstance(
                         x, (int, float)) for x in poly) or poly == []:
         return None
+    if len(poly) == 1 or len(poly)== 0:
+        return intg_list
     for i in range(len(poly)):
         x = poly[i] % (i + 1)
         y = poly[i] / (i+1)
-        if (x != 0 and y != 0):
+        if (x != 0):
             intg_list.append(y)
         else:
             intg_list.append(int(y))
