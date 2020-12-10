65d9c8078afdd85215186274db05d41db5cb625f
Immaannn2222 <imennaayari@gmail.com>
2020-12-10 20:54:58 +0100

    fix

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -2,8 +2,8 @@
 """ 9th task """
 
 
-def summation_i_squared(n): 
+def summation_i_squared(n):
     """ calculates sum of i^2 """
-    if not isinstance (n, (int, float)) or n < 1:
+    if not isinstance(n, (int, float)) or n < 1:
         return None
-    return  (n * (n + 1) * (2 * n + 1)) // 6
+    return (n * (n + 1) * (2 * n + 1)) // 6
