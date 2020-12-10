39641f76ad56b44740bd56b18a8b47e3d042316d
Immaannn2222 <imennaayari@gmail.com>
2020-12-10 20:43:23 +0100

    recorrect

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -6,6 +6,6 @@ def poly_derivative(poly):
     """calculates the derivative of a polynomial"""
     if poly == [0] or len(poly) < 1:
         return [0]
-    if not isinstance(enumerate(poly[:1], 0), int) or not isinstance(poly, list) or poly == []:
+    if not isinstance(enumerate(poly[:1], 0), (int, float)) or not isinstance(poly, list) or poly == []:
         return [i * j for i, j in enumerate(poly[1:], 1)]
     return None
--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -4,6 +4,6 @@
 
 def summation_i_squared(n): 
     """calculates sum of i^2"""
-    if isinstance (n, int) is not True or n < 0:
+    if not isinstance (n, (int, float)) or n < 0:
         return None
     return  (n * (n + 1) * (2 * n + 1)) // 6
