821141c315eb31b3b24b668009792c364f6cac25
Immaannn2222 <imennaayari@gmail.com>
2020-12-13 21:53:20 +0100

    mig

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -6,6 +6,4 @@ def poly_derivative(poly):
     """ calculates the derivative of a polynomial """
     if poly is None:
         return None
-    if len(poly) <= 1:
-        return [0]
     return [i * j for i, j in enumerate(poly[1:], 1) if type(j) is int]
