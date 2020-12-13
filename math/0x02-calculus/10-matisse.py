02e301fcb313e85576c843a500f8807d64f2be48
Immaannn2222 <imennaayari@gmail.com>
2020-12-13 21:58:38 +0100

    fix conditions

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -4,6 +4,8 @@
 
 def poly_derivative(poly):
     """ calculates the derivative of a polynomial """
-    if poly is None:
+    if poly is None or isinstance(poly, list) is not True:
         return None
+    if len(poly) == 1:
+        return [0]
     return [i * j for i, j in enumerate(poly[1:], 1) if type(j) is int]
