9cbac3a90263d024ed3f34d13dd1d101196b682f
Immaannn2222 <imennaayari@gmail.com>
2020-12-15 20:45:52 +0100

    add task 1

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -14,6 +14,17 @@ class Poisson:
         else:
             if isinstance(data, list) is False:
                 raise TypeError("data must be a list")
-            if(len(data)) < 1:
+            if(len(data)) < 3:
                 raise ValueError("data must contain multiple values")
             self.lambtha = (sum(data) / len(data))
+
+    def pmf(self, k):
+        e = 2.7182818285
+        fact = 1
+        """ Calculates the value of the PMF of “successes”"""
+        if k is not int:
+            int(k)
+        if k > 0:
+            for i in range(1, k + 1):
+                fact *= i
+        return((e **(-self.lambtha) * self.lambtha ** k) / fact)
