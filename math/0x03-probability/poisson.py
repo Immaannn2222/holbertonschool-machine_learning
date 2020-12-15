167645148adc7ebd6d2e7fbe3495d1907662b446
Immaannn2222 <imennaayari@gmail.com>
2020-12-15 19:40:49 +0100

    push 1st task

---
+++ math/0x03-probability/poisson.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" task 0 """
+
+
+class Poisson:
+    """ represents a poisson distribution """
+    def __init__(self, data=None, lambtha=1.):
+        """ class constructor """
+        if data is None:
+            if (lambtha <= 0):
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = lambtha
+        else:    
+            if isinstance (data, list) is False:
+                raise TypeError("data must be a list")
+            if(len(data)) < 1:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = float(sum(data) / len(data))
