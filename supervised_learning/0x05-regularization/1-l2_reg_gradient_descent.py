2d8662728d5d1bc432aee393c2d372468a12a40c
Immaannn2222 <imennaayari@gmail.com>
2021-01-21 22:04:15 +0100

    fix ambigious

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -5,15 +5,15 @@ import numpy as np
 
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     """updates gradient descent with L2 regularization"""
-    l = len(Y[0])
+    leng = len(Y[0])
     dZ = cache["A" + str(L)] - Y
     for i in range(L, 0, -1):
         Act = cache["A" + str(i - 1)]
-        dW = np.matmul(dZ, Act.T) / l
-        db = np.sum(dZ, axis=1, keepdims=True) / l
+        dW = np.matmul(dZ, Act.T) / leng
+        db = np.sum(dZ, axis=1, keepdims=True) / leng
         tanh = 1 - Act * Act
         dZ = np.matmul(weights[
             "W" + str(i)].T, dZ) * tanh
-        dW_L2reg = dW + (lambtha/l)*weights["W" + str(i)]
+        dW_L2reg = dW + (lambtha/leng)*weights["W" + str(i)]
         weights["W" + str(i)] -= alpha * dW_L2reg
         weights["b" + str(i)] -= alpha * db
