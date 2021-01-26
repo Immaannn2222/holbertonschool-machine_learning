34b76c2ad0073a3139d62ea1789b77e13f055a93
Immaannn2222 <imennaayari@gmail.com>
2021-01-26 10:51:21 +0100

    fix pep8

--- supervised_learning/0x06-keras/2-optimize.py
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -4,7 +4,7 @@ import tensorflow.keras as K
 
 
 def optimize_model(network, alpha, beta1, beta2):
-    """sets up Adam optimization for a keras model with categorical crossentropy loss and accuracy metrics"""
+    """keras model with categorical crossentropy loss and accuracy metrics"""
     network.compile(
         optimizer=K.optimizers.Adam(
             alpha,
