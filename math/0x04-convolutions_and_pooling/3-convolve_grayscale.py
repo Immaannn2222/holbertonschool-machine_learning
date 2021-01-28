9115a8c80905456c3081d5dec553ebe644f7f28a
Immaannn2222 <imennaayari@gmail.com>
2021-01-28 20:41:29 +0100

    cehc

--- math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
+++ math/0x04-convolutions_and_pooling/3-convolve_grayscale.py
@@ -3,47 +3,39 @@
 import numpy as np
 
 
-
 def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
     """performs a convolution on grajscale images"""
     m, h, w = images.shape
     kh, kw = kernel.shape
-    sh = stride[0]
-    sw = stride[1]
+    sh, sw = stride
     if padding == 'valid':
-        o_h = np.floor((h - kh + 1) / sh)
-        o_w = np.floor((w - kw + 1) / sw)
-        output = np.zeros((m, o_h, o_w))
-        pad_images = images
-    if padding == "same":
-        o_h = h // sh
-        o_w = kw // sw
-        if kh % 2 == 1:
-            p_h = (kh - 1) // 2
-        else:
-            p_h = kh // 2
-        if kw % 2 == 1:
-            p_w = (kw - 1) // 2
-        else:
-            p_w = kw // 2
+        p_h = 0
+        p_w = 0
+        padded_img = images
 
-        pad_images = np.pad(
-            array=images,
-            pad_width=((0,), (p_h,), (p_w,)),
-            mode="constant")
-        output = np.zeros((m, o_h, o_w))
-    if padding is tuple:
-        o_h = h - kh + 2 * padding[0] + 1
-        o_w = w - kw + 2 * padding[1] + 1
-        output = np.zeros((m, o_h, o_w))
-        pad_images = np.pad(
-            array=images,
-            pad_width=((0,), (padding[0],), (padding[1],)),
-            mode="constant")
+    elif padding == 'same':
+        p_h = int(np.floor(((h - 1) * sh + kh - h) / 2))
+        p_w = int(np.floor(((w - 1) * sw + kw - w) / 2))
+
+        output = np.zeros((m, h, w))
+
+    else:
+        p_h = padding[0]
+        p_w = padding[1]
+
+    o_h = int(np.floor((h - kh + 2 * p_h) / sh) + 1)
+    o_w = int(np.floor((w - kw + 2 * p_w) / sw) + 1)
+
+    output = np.zeros((m, o_h, o_w))
+    padded_img = np.pad(
+        array=images,
+        pad_width=((0,), (p_h,), (p_w,)),
+        mode="constant",
+        constant_values=0)
+
     for x in range(o_h):
         for y in range(o_w):
-            output[:, x, y] = np.sum(
-                kernel * pad_images[:, x * sh:x * sh + kh, y * sw:y * sw + kw],
-                axis=(1, 2)
-            )
+            output[:, x, y] = (
+                padded_img[:, x * sh:kh + x * sh, y * sw:kw + y * sw] * kernel
+                               ).sum(axis=(1, 2))
     return output
