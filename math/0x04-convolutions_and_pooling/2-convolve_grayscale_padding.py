0fae9d8b3c702ca1eac8c6cd927645d61756d81c
Immaannn2222 <imennaayari@gmail.com>
2021-01-28 14:54:41 +0100

    ceck

---
+++ math/0x04-convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Convolve"""
+import numpy as np
+
+
+def convolve_grayscale_padding(images, kernel, padding):
+    """performs a convolution on grayscale images with custom padding"""
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+    o_h = h + padding[0]
+    o_w = w + padding[1]
+    output = np.zeros((m, o_h, o_w))
+    pad_images = np.pad(
+        array=images,
+        pad_width=((0,), (padding[0],), (padding[1],)),
+        mode="constant")
+    for x in range(o_h):
+        for y in range(o_w):
+            output[:, x, y] = np.sum(
+                kernel * pad_images[:, x:x + kh, y:y + kw],
+                axis=(1, 2)
+            )
+    return output
