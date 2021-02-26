196b5701a924107366715c1817f31cb600a53a15
Immaannn2222 <imennaayari@gmail.com>
2021-02-26 15:38:25 +0100

    push first task

---
+++ supervised_learning/0x0A-object_detection/0-yolo.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""yolo"""
+import tensorflow.keras as K
+
+
+class Yolo:
+    """class Yolo that uses the Yolo v3 algorithm"""
+
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        """class constructor"""
+        self.model = K.models.load_model(filepath=model_path)
+        f = open(classes_path, "r")
+        self.class_names = [lines.split('\n')[0] for lines in f.readlines()]
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
