a005022e8c8cb7f1820cc5eb95254bc55b723cb3
Immaannn2222 <imennaayari@gmail.com>
2021-03-14 21:43:32 +0100

    push this file

--- supervised_learning/0x0A-object_detection/1-yolo.py
+++ supervised_learning/0x0A-object_detection/1-yolo.py
@@ -15,41 +15,41 @@ class Yolo:
         self.nms_t = nms_t
         self.anchors = anchors
 
-    def sigmoid(self, x):
-        """implemnets the sigmoid function"""
-        return 1 / (1 + np.exp(x))
+    def sigmoid(self, z):
+        """ sigmoid function"""
+        return (1 / (1 + np.exp(-z)))
 
     def process_outputs(self, outputs, image_size):
-        """process outputs"""
+        """ Function that returns a tuple of (boxes, box_confidences,
+           box_class_probs) """
         boxes = []
-        box_confidences = []
-        box_class_probs = []
+        confidences = []
+        cls_probs = []
         img_h, img_w = image_size
-
         for output in outputs:
             boxes.append(output[..., 0:4])
-            box_confidences.append(self.sigmoid(output[..., 4, np.newaxis]))
-            box_class_probs.append(self.sigmoid(output[..., 5:]))
-        for i, box in enumerate(boxes):
-            gr_h, gr_w, anchors_boxes, _ = box.shape
+            confidences.append(self.sigmoid(output[..., 4, np.newaxis]))
+            cls_probs.append(self.sigmoid(output[..., 5:]))
+        for i, j in enumerate(boxes):
+            gr_h, gr_w, anchors_boxes, _ = j.shape
             cx = np.indices((gr_h, gr_w, anchors_boxes))[1]
             cy = np.indices((gr_h, gr_w, anchors_boxes))[0]
-            t_x = box[..., 0]
-            t_y = box[..., 1]
-            t_w = box[..., 2]
-            t_h = box[..., 3]
-            p_w = self.anchors[i, :, 0]
-            p_h = self.anchors[i, :, 1]
+            t_x = j[..., 0]
+            t_y = j[..., 1]
+            width = j[..., 2]
+            height = j[..., 3]
+            anc_w = self.anchors[i, :, 0]
+            anc_h = self.anchors[i, :, 1]
             bx = (self.sigmoid(t_x) + cx) / gr_w
             by = (self.sigmoid(t_y) + cy) / gr_h
-            bw = (np.exp(t_w) * p_w) / self.model.input.shape[1].value
-            bh = (np.exp(t_h) * p_h) / self.model.input.shape[2].value
-            tl_x = bx - bw / 2
-            tl_y = by - bh / 2
-            br_x = bx + bw / 2
-            br_y = by + bh / 2
-            box[..., 0] = tl_x * img_w
-            box[..., 1] = tl_y * img_h
-            box[..., 2] = br_x * img_w
-            box[..., 3] = br_y * img_h
-        return boxes, box_confidences, box_class_probs
+            bw = (np.exp(width) * anc_w) / self.model.input.shape[1].value
+            bh = (np.exp(height) * anc_h) / self.model.input.shape[2].value
+            x1 = bx - bw / 2
+            y1 = by - bh / 2
+            x2 = x1 + bw
+            y2 = y1 + bh
+            j[..., 0] = x1 * img_w
+            j[..., 1] = y1 * img_h
+            j[..., 2] = x2 * img_w
+            j[..., 3] = y2 * img_h
+        return boxes, confidences, cls_probs
