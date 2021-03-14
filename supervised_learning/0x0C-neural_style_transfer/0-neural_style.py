9d2e9f44fdf8abcbf3040b7f870d34dd10055874
Immaannn2222 <imennaayari@gmail.com>
2021-03-14 18:43:50 +0100

    condition

--- supervised_learning/0x0C-neural_style_transfer/0-neural_style.py
+++ supervised_learning/0x0C-neural_style_transfer/0-neural_style.py
@@ -18,11 +18,15 @@ class NST():
     def __init__(self, style_image, content_image, alpha=1e4, beta=1):
         """the class constructor"""
         if not isinstance(
-                style_image, np.ndarray) or style_image.shape[2] != 3:
+            style_image,
+            np.ndarray) or \
+                style_image.ndim != 3 and style_image.shape[2] != 3:
             raise TypeError(
                 "style_image must be a numpy.ndarray with shape (h, w, 3)")
         if not isinstance(
-                content_image, np.ndarray) or content_image.shape[2] != 3:
+            content_image,
+            np.ndarray) or \
+                content_image.ndim != 3 and content_image.shape[2] != 3:
             raise TypeError(
                 "content_image must be a numpy.ndarray with shape (h, w, 3)")
         if not isinstance(alpha, (float, int)) or alpha < 0:
