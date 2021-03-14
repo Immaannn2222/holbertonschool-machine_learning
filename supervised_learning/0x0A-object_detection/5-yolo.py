d507a13968c5c7ead07bad96af142fee7f570568
Immaannn2222 <imennaayari@gmail.com>
2021-03-14 21:59:35 +0100

    rectify 5

--- supervised_learning/0x0A-object_detection/5-yolo.py
+++ supervised_learning/0x0A-object_detection/5-yolo.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 """OBJECT DETECTION YOLOV3"""
-from tensorflow import keras as K
 import numpy as np
+from tensorflow import keras as K
 import cv2
+import glob
 
 
 class Yolo:
-    """uses the Yolo v3 algorithm to perform object detection"""
+    """class Yolo that uses the Yolo v3 algorithm"""
+
     def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
         """class constructor"""
         self.model = K.models.load_model(model_path)
@@ -16,61 +18,67 @@ class Yolo:
         self.nms_t = nms_t
         self.anchors = anchors
 
-    def sigmoid(self, z):
-        """ sigmoid function"""
-        return (1 / (1 + np.exp(-z)))
+    def sigmoid(self, x):
+        """implemnets the sigmoid function"""
+        return 1 / (1 + np.exp(x))
 
     def process_outputs(self, outputs, image_size):
-        """ Function that returns a tuple of (boxes, box_confidences,
-           box_class_probs) """
+        """procees the output of a draknet model for a single image"""
         boxes = []
-        confidences = []
-        cls_probs = []
-        img_h, img_w = image_size
+        box_conf = []
+        cls_proba = []
+        image_height, image_width = image_size
         for output in outputs:
             boxes.append(output[..., 0:4])
-            confidences.append(self.sigmoid(output[..., 4, np.newaxis]))
-            cls_probs.append(self.sigmoid(output[..., 5:]))
-        for i, j in enumerate(boxes):
-            gr_h, gr_w, anchors_boxes, _ = j.y
-            cx = np.indices((gr_h, gr_w, anchors_boxes))[1]
-            cy = np.indices((gr_h, gr_w, anchors_boxes))[0]
-            t_x = j[..., 0]
-            t_y = j[..., 1]
-            width = j[..., 2]
-            height = j[..., 3]
-            anc_w = self.anchors[i, :, 0]
-            anc_h = self.anchors[i, :, 1]
-            bx = (self.sigmoid(t_x) + cx) / gr_w
-            by = (self.sigmoid(t_y) + cy) / gr_h
-            bw = (np.exp(width) * anc_w) / self.model.input.y[1].value
-            bh = (np.exp(height) * anc_h) / self.model.input.y[2].value
+            box_conf.append(self.sigmoid(output[..., 4]))
+            cls_proba.append(self.sigmoid(output[..., 5:]))
+        for idx, b in enumerate(boxes):
+            grid_h, grid_w, anchors_boxes, _ = b.shape
+            cx = np.indices((grid_h, grid_w, anchors_boxes))[1]
+            cy = np.indices((grid_h, grid_w, anchors_boxes))[0]
+            tx = b[..., 0]
+            ty = b[..., 1]
+            width = b[..., 2]
+            height = b[..., 3]
+            anc_w = self.anchors[idx, :, 0]
+            anc_h = self.anchors[idx, :, 1]
+            bx = (self.sigmoid(tx) + cx) / grid_w
+            by = (self.sigmoid(ty) + cy) / grid_h
+            bw = (np.exp(width) * anc_w) / self.model.input.shape[1].value
+            bh = (np.exp(height) * anc_h) / self.model.input.shape[2].value
             x1 = bx - bw / 2
             y1 = by - bh / 2
             x2 = x1 + bw
             y2 = y1 + bh
-            j[..., 0] = x1 * img_w
-            j[..., 1] = y1 * img_h
-            j[..., 2] = x2 * img_w
-            j[..., 3] = y2 * img_h
-        return boxes, confidences, cls_probs
+            b[..., 0] = x1 * image_width
+            b[..., 1] = y1 * image_height
+            b[..., 2] = x2 * image_width
+            b[..., 3] = y2 * image_height
+        return boxes, box_conf, cls_proba
+
+    def filter_boxes(self, boxes, box_confidences, box_class_probs):
+        """ filter boxes """
+
+    def non_max_suppression(self, filtered_boxes, box_classes, box_scores):
+        """applies non max suppression"""
 
     @staticmethod
     def load_images(folder_path):
         """ imports images using cv"""
-        imgs = []
-        imgs_paths = []
+        is = []
+        is_paths = []
         for path in glob.glob(folder_path + '/*'):
-            imgs.append(cv2.imread(path))
-            imgs_paths.append(path)
-        return imgs, imgs_paths
+            is.append(cv2.imread(path))
+            is_paths.append(path)
+        return is, is_paths
 
     def preprocess_images(self, images):
-        """Resize the images with inter-cubic interpolation and Rescale"""
+        """Resize the images with inter-cubic interpolation
+            Rescale all images to have pixel values in the range [0, 1] """
         x = []
         y = []
-        img_w = self.model.input.y[1].value
-        img_h = self.model.input.y[2].value
+        img_w = self.model.input.shape[1].value
+        img_h = self.model.input.shape[2].value
         for i in images:
             image = cv2.resize(i, (img_w, img_h),
                                interpolation=cv2.INTER_CUBIC)
