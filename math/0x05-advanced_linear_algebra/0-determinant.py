62b0eb982c42e06ac2d0f5bcb3e9c90532f66603
Immaannn2222 <imennaayari@gmail.com>
2021-03-17 17:18:43 +0100

    change condition

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -4,15 +4,17 @@
 
 def determinant(matrix):
     """function that calculates the determinant of a matrix"""
-    if not isinstance(matrix, list) or matrix[0] == []:
+    if not isinstance(matrix, list):
         raise TypeError('matrix must be a list of lists')
-    if len(matrix) != len(matrix[0]):
-        raise ValueError('matrix must be a square matrix')
+    if not all(isinstance(i, list) for i in matrix):
+        raise TypeError('matrix must be a list of lists')
     if matrix == [[]] or len(matrix[0]) == 0:
         return 1
     if len(matrix[0]) == 1:
         return matrix[0][0]
-    if len(matrix) == 2:
+    if len(matrix) != len(matrix[0]):
+        raise ValueError('matrix must be a square matrix')
+    if len(matrix) == 2 and len(matrix[0]) == 2:
         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
     det = 0
     n = len(matrix)
