6dc73f63db49d1b5df63c9c4972bcf7586b1b40a
Immaannn2222 <imennaayari@gmail.com>
2021-03-17 23:20:46 +0100

    pff

---
+++ math/0x05-advanced_linear_algebra/1-minor.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""advanced linear algebra"""
+
+
+def determinant(matrix):
+    """calculates the determinant of a matrix"""
+    if not isinstance(matrix, list) or matrix == []:
+        raise TypeError('matrix must be a list of lists')
+    if any(not isinstance(i, list) for i in matrix):
+        raise TypeError('matrix must be a list of lists')
+    if matrix == [[]]:
+        return 1
+    if any(len(i) != len(matrix) for i in matrix):
+        raise ValueError('matrix must be a square matrix')
+    n = len(matrix)
+    if n == 1:
+        return matrix[0][0]
+    # calculate the determinant of 2x2 matrix
+    if n == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+    det = 0
+    for i in range(n):
+        det += (-1) ** i * matrix[0][i] * determinant(reduce_mat(matrix, i))
+    return det
+
+
+def reduce_mat(matrix, x):
+    """returns reduced matrix starting after the first linz"""
+    # reducing the matrix/ elimminating column
+    return [col[:x] + col[x + 1:] for col in (matrix[1:])]
+
+# def reduce_mat_double(matrix, x, y):
+#     """returns reduced matrix"""
+#     # reducing the matrix/ elimminating column
+#     return [col[:x] + col[x + 1:] for col in (matrix[:y] + matrix[y + 1:])]
+
+
+def minor(matrix):
+    """calculates the minor matrix of a matrix"""
+    if not isinstance(matrix, list) or matrix == []:
+        raise TypeError('matrix must be a list of lists')
+    if any(not isinstance(i, list) for i in matrix):
+        raise TypeError('matrix must be a list of lists')
+    if matrix == [[]]:
+        return 1
+    if any(len(i) != len(matrix) for i in matrix) or len(matrix[0]) == 0:
+        raise ValueError('matrix must be a non-empty square matrix')
+    n = len(matrix)
+    if n == 1:
+        return matrix[0][0]
+    if n == 2:
+        return [elem[::-1] for elem in matrix][::-1]
+    else:
+        for i in range(n):
+            for j in range(n):
+                new_mat = [col[:j] + col[j + 1:] for col in (matrix[:i] + matrix[i + 1:])]
+        return determinant(new_mat)
