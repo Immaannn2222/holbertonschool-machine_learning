96049b9096bcad7c9eabb28a9aa10400286f71a5
Immaannn2222 <imennaayari@gmail.com>
2021-03-17 20:15:04 +0100

    mchech the recursive methos

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -8,7 +8,23 @@ def determinant(matrix):
         raise TypeError('matrix must be a list of lists')
     if any(not isinstance(i, list) for i in matrix):
         raise TypeError('matrix must be a list of lists')
-    if any(len(i) != len(matrix) for i in matrix):
-        raise ValueError('matrix must be a square matrix')
     if matrix == [[]]:
         return 1
+    if any(len(i) != len(matrix) for i in matrix):
+        raise ValueError('matrix must be a square matrix')
+    n = len(matrix)
+    if n == 1:
+        return matrix[0][0]
+    # calculate the determinant of 2x2 matrix
+    if n == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+    det = 0
+    for i in range(n):
+        det += (-1) ** i * matrix[0][i] * determinant(reduce_mat(matrix, i))
+    return det
+
+
+def reduce_mat(matrix, x):
+    """returns reduced matrix starting after the first linz"""
+    # reducing the matrix/ elimminating column
+    return [col[:x] + col[x + 1:] for col in (matrix[1:])]
