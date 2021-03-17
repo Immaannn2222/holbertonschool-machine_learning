062232a4398ca2fa06eaaddd186281fb4224e135
Immaannn2222 <imennaayari@gmail.com>
2021-03-17 18:24:38 +0100

    initial

--- math/0x05-advanced_linear_algebra/0-determinant.py
+++ math/0x05-advanced_linear_algebra/0-determinant.py
@@ -12,17 +12,3 @@ def determinant(matrix):
         raise ValueError('matrix must be a square matrix')
     if matrix == [[]]:
         return 1
-    lenth_m = len(matrix)
-    matrix_cpy = matrix.copy()
-    for d in range(lenth_m):
-        for i in range(d + 1, lenth_m):
-            if matrix_cpy[d][d] == 0:
-                matrix_cpy[d][d] == 1e-18
-            moving_s = matrix_cpy[i][d] / matrix_cpy[d][d]
-            for j in range(lenth_m):
-                matrix_cpy[i][j] = matrix_cpy[i][j] - movi\
-                    ng_s * matrix_cpy[d][j]
-    det = 1
-    for i in range(lenth_m):
-        det *= matrix_cpy[i][i]
-    return det
