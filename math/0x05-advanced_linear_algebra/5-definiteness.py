a030f71d1845db7790aca19fcdbdc7724bd767b1
Immaannn2222 <imennaayari@gmail.com>
2021-03-18 21:06:07 +0100

    check

---
+++ math/0x05-advanced_linear_algebra/5-definiteness.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""advanced linear algebra"""
+import numpy as np
+
+
+def definiteness(matrix):
+    """calculates the definiteness of a matrix"""
+    if not isinstance(matrix, np.ndarray):
+        raise TypeError('matrix must be a numpy.ndarray')
+    if not np.array_equal(matrix, matrix.T):
+        return None
+
+        if np.all(np.linalg.eigvals(matrix) > 0):
+            return("Positive definite")
+        elif np.all(np.linalg.eigvals(matrix) < 0):
+            return("Negative definite")
+        elif np.all(np.linalg.eigvals(matrix) >= 0):
+            return("Positive semi-definite")
+        elif np.all(np.linalg.eigvals(matrix) <= 0):
+            return("Negative semi-definite")
+        else:
+            return("Indefinite")
