e738eac38d66784eba60f20f5be20481c95cd875
Immaannn2222 <imennaayari@gmail.com>
2021-03-22 21:09:26 +0100

    fix

--- math/0x06-multivariate_prob/1-correlation.py
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -5,7 +5,6 @@ import numpy as np
 
 def correlation(C):
     """calculates a correlation matrix"""
-    d, d = C.shape
     if not isinstance(C, np.ndarray):
         raise TypeError("C must be a numpy.ndarray")
     if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
