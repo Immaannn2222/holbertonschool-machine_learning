01e3a3b3a371d392f57333c5e9fd878489999783
Immaannn2222 <imennaayari@gmail.com>
2021-03-22 19:34:13 +0100

    correct dimension

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -6,10 +6,10 @@ import numpy as np
 def mean_cov(X):
     """calculates the mean and covariance of a data set"""
     n, d = X.shape
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
         raise TypeError("X must be a 2D numpy.ndarray")
     if n < 2:
         raise ValueError("X must contain multiple data points")
-    mean = np.mean(X, axis=0)
+    mean = np.mean(X, axis=0, keepdims=True)
     cov = np.matmul((X - mean).T, (X - mean)) / (n - 1)
     return mean, cov
