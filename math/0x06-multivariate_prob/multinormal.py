9e19dd7546b86ed6f6d8ce85cf5d65df53bef3a6
Immaannn2222 <imennaayari@gmail.com>
2021-03-22 23:29:40 +0100

    check

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -15,3 +15,15 @@ class MultiNormal:
         self.mean = np.mean(data, axis=1, keepdims=True)
         Y = data - self.mean
         self.cov = (np.dot(Y, Y.T)) / (n - 1)
+
+    def pdf(self, x):
+        """calculates the PDF at a data point"""
+        d, _ = self.cov.shape
+        if not isinstance(x, np.ndarray):
+            raise TypeError('x must be a numpy.ndarray')
+        if x.ndim != 2 or (x.shape[0] != d)or (x.shape[1] != 1):
+            raise ValueError("x must have the shape ({}, 1)".format(d))
+        x_m = x - self.mean
+        sqrt = np.sqrt((2 * np.pi)**d * np.linalg.det(self.cov))
+        exp = np.exp(-(np.linalg.solve(self.cov, x_m).T.dot(x_m)) / 2)
+        return (1 / (sqrt) * exp)[0][0]
