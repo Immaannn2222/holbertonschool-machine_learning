fe292d7baaf9b3ecb6f929dce352c5d10d135ebe
Immaannn2222 <imennaayari@gmail.com>
2021-03-22 21:06:16 +0100

    fix

--- math/0x06-multivariate_prob/1-correlation.py
+++ math/0x06-multivariate_prob/1-correlation.py
@@ -5,10 +5,9 @@ import numpy as np
 
 def correlation(C):
     """calculates a correlation matrix"""
-    d, d = C.shape
     if not isinstance(C, np.ndarray):
         raise TypeError("C must be a numpy.ndarray")
-    if C.ndim != 2 or C.shape[0] != C.shape[1]:
+    if len(C.shape) != 2 or C.shape[0] != C.shape[1]:
         raise ValueError("C must be a 2D square matrix")
     X = np.sqrt(np.diag(C))
     corr = 1 / (np.outer(X, X) / C)
