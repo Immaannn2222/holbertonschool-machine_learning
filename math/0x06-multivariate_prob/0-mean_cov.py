96f45890bfb32b93444f610271d243e4e8924722
Immaannn2222 <imennaayari@gmail.com>
2021-03-22 19:29:39 +0100

    verify the condition

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -6,7 +6,7 @@ import numpy as np
 def mean_cov(X):
     """calculates the mean and covariance of a data set"""
     n, d = X.shape
-    if not isinstance(X, np.ndarray) or X.ndim != 2:
+    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         raise TypeError("X must be a 2D numpy.ndarray")
     if n < 2:
         raise ValueError("X must contain multiple data points")
