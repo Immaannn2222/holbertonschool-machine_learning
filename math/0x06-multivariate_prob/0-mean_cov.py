ab9d912bc916726ff71468a4b53e620b5e8a4298
Immaannn2222 <imennaayari@gmail.com>
2021-03-22 21:10:38 +0100

    rmove the shape line

--- math/0x06-multivariate_prob/0-mean_cov.py
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -5,11 +5,11 @@ import numpy as np
 
 def mean_cov(X):
     """calculates the mean and covariance of a data set"""
-    n, d = X.shape
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         raise TypeError("X must be a 2D numpy.ndarray")
+    n, d = X.shape
     if n < 2:
         raise ValueError("X must contain multiple data points")
     mean = np.mean(X, axis=0, keepdims=True)
-    cov = (np.dot((X - mean).T, (X - mean))) / (n - 1)
+    cov = (np.matmul((X - mean).T, (X - mean))) / (n - 1)
     return mean, cov
