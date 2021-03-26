d7f5eeb3560251da58f51f6714edf437186832ff
Immaannn2222 <imennaayari@gmail.com>
2021-03-26 11:53:05 +0100

    fix

--- math/0x07-bayesian_prob/0-likelihood.py
+++ math/0x07-bayesian_prob/0-likelihood.py
@@ -6,12 +6,11 @@ from math import factorial as f
 
 def likelihood(x, n, P):
     """calculates the likelihood of data various hypothetical probabilities"""
-    if not isinstance(x, int) or n < 1:
-        raise ValueError("n must be a positive integer")
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError('n must be a positive integer')
     if not isinstance(x, int) or x < 0:
         raise ValueError(
-            "x must be an integer that is greater than or equal to 0")
-    if x > n:
+            'x must be an integer that is greater than or equal to 0')
         raise ValueError("x cannot be greater than n")
     if not isinstance(P, np.ndarray) or P.ndim != 1:
         raise TypeError("P must be a 1D numpy.ndarray")
