621bfe18ff31e0bae81156bf6c9a246e7879abb5
Immaannn2222 <imennaayari@gmail.com>
2021-03-28 20:21:22 +0100

    fix

--- math/0x07-bayesian_prob/100-continuous.py
+++ math/0x07-bayesian_prob/100-continuous.py
@@ -13,9 +13,9 @@ def posterior(x, n, p1, p2):
             "x must be an integer that is greater than or equal to 0")
     if x > n:
         raise ValueError('x cannot be greater than n')
-    if not isinstance(p1, float) or 0 > p1 > 1:
+    if not isinstance(p1, float) or p1 < 0 or p1 > 1:
         raise ValueError("p1 must be a float in the range [0, 1]")
-    if not isinstance(p2, float) or 0 > p2 > 1:
+    if not isinstance(p2, float) or p2 < 0 or p2 > 1:
         raise ValueError("p2 must be a float in the range [0, 1]")
     if p2 <= p1:
         raise ValueError("p2 must be greater than p1")
