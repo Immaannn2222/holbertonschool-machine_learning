a7c033753cfe0cf0a4a68f94d706ab7de1f175aa
Immaannn2222 <imennaayari@gmail.com>
2021-03-28 20:15:43 +0100

    add the main fucntion

--- math/0x07-bayesian_prob/100-continuous.py
+++ math/0x07-bayesian_prob/100-continuous.py
@@ -42,3 +42,6 @@ def posterior(x, n, p1, p2):
         raise ValueError("p2 must be a float in the range [0, 1]")
     if p2 <= p1:
         raise ValueError("p2 must be greater than p1")
+    y = special.btdtr(x+1, n-x+1, p1)
+    z = special.btdtr(x+1, n-x+1, p2)
+    return z - y
