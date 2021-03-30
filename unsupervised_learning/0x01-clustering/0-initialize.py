c66cf6b759872aeb19bb07e25326a137577609ae
Immaannn2222 <imennaayari@gmail.com>
2021-03-30 10:33:53 +0100

    check this styl

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -6,9 +6,9 @@ import numpy as np
 def initialize(X, k):
     """initializes cluster centroids for K-means"""
     try:
-        n, d = X.shape
         low_b = np.min(X, axis=0)
         high_b = np.max(X, axis=0)
+        n, d = X.shape
         centroids = np.random.uniform(low_b, high_b, (k, d))
     except Exception:
         return None
