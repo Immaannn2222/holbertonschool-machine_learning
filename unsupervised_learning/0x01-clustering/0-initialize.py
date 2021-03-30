044e0ce4d354e7481a827a83d63a2cf54c8257de
Immaannn2222 <imennaayari@gmail.com>
2021-03-30 10:39:50 +0100

    try with no test

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -5,8 +5,8 @@ import numpy as np
 
 def initialize(X, k):
     """initializes cluster centroids for K-means"""
-    # if not isinstance(k, int) or k <= 0:
-    #     return None
+    if not isinstance(k, int) or k <= 0:
+        return None
     try:
         n, d = X.shape
         low_b = np.min(X, axis=0)
