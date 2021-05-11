704f3e207bd45eff44734334e7c4c2d4c6a40abf
Immaannn2222 <imennaayari@gmail.com>
2021-05-11 21:53:15 +0100

    task 3

--- supervised_learning/0x0F-word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/0x0F-word_embeddings/3-gensim_to_keras.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """NLP MODULE"""
-from keras.layers.embeddings import Embedding
 
 
 def gensim_to_keras(model):
     """converts a gensim word2vec model to a keras Embedding layer"""
+    emebed_layer = model.wv.get_keras_embedding(train_embeddings=False)
+    return emebed_layer
