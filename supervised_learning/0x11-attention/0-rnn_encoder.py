5234a76ada02e9f4302d28bb9ed43ece9fa497fb
Immaannn2222 <imennaayari@gmail.com>
2021-05-21 20:37:34 +0100

    fix

--- supervised_learning/0x11-attention/0-rnn_encoder.py
+++ supervised_learning/0x11-attention/0-rnn_encoder.py
@@ -7,20 +7,21 @@ class RNNEncoder(tf.keras.layers.Layer):
     """encode for machine translation"""
     def __init__(self, vocab, embedding, units, batch):
         """class constructor"""
-        super(RNNEncoder, self).__init__()
-        self.batch = batch
-        self.units = units
-        self.embedding = tf.keras.layers.Embedding(vocab, embedding)
-        self.gru = tf.keras.layers.GRU(
-            units=units, kernel_initializer='glorot_uniform',
-            return_sequences=True, return_state=True)
+        def __init__(self, vocab, embedding, units, batch):
+            """ initialization """
+            super(RNNEncoder, self).__init__()
+            self.batch = batch
+            self.units = units
+            self.embedding = tf.keras.layers.Embedding(vocab, embedding)
+            self.gru = tf.keras.layers.GRU(
+                units, recurrent_initializer="glorot_uniform",
+                return_sequences=True, return_state=True)
 
     def initialize_hidden_state(self):
         """Initializes the hidden states to a tensor of zeros"""
         return tf.zeros((self.batch, self.units))
 
     def call(self, x, initial):
-        """call function"""
-        embedded = self.embedding(x)
-        outputs, hidden = self.gru(embedded, initial)
-        return outputs, hidden
+        """call funtion """
+        x = self.embedding(x)
+        return self.gru(x, initial)
--- supervised_learning/0x11-attention/2-rnn_decoder.py
+++ supervised_learning/0x11-attention/2-rnn_decoder.py
@@ -7,23 +7,24 @@ SelfAttention = __import__('1-self_attention').SelfAttention
 class RNNDecoder(tf.keras.layers.Layer):
     """ decode for machine translation"""
     def __init__(self, vocab, embedding, units, batch):
-        """class constructor"""
+        """ initialization """
         super(RNNDecoder, self).__init__()
+        self.batch = batch
+        self.units = units
         self.embedding = tf.keras.layers.Embedding(vocab, embedding)
         self.gru = tf.keras.layers.GRU(
-            units=units, kernel_initializer='glorot_uniform',
+            units, recurrent_initializer="glorot_uniform",
             return_sequences=True, return_state=True)
         self.F = tf.keras.layers.Dense(vocab)
-        self.attention = SelfAttention(units)
+        self.attention = SelfAttention(self.units)
 
     def call(self, x, s_prev, hidden_states):
-        """public instance method"""
-        context_vector, attention_weights = self.attention(
-            s_prev, hidden_states)
+        """call funtion """
+        context_vector, attention_w = self.attention(s_prev, hidden_states)
         x = self.embedding(x)
-        x = tf.concat([tf.expand_dims(
-            context_vector, 1), x], axis=-1)
-        output, state = self.gru(x)
+        x = tf.concat([tf.expand_dims(context_vector, 1), x], axis=-1)
+        output, s = self.gru(x)
         output = tf.reshape(output, (-1, output.shape[2]))
-        x = self.F(output)
-        return x, state
+        y = self.F(output)
+
+        return y, s
