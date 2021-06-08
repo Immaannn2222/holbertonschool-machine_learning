5eb1d2ea69494d1296d73827847cb9ab0a6319b3
Immaannn2222 <imennaayari@gmail.com>
2021-06-08 14:06:51 +0100

    check this task

--- unsupervised_learning/0x02-hmm/1-regular.py
+++ unsupervised_learning/0x02-hmm/1-regular.py
@@ -4,16 +4,21 @@ import numpy as np
 
 
 def regular(P):
-    """dfghjkluytertyuio"""
+    """determines the steady state probabilities of a regular markov chain"""
     n, m = np.shape(P)
+    if not isinstance(P, np.ndarray) or len(P.shape) != 2:
+        return None
+    if n != m:
+        return None
     if (P == np.eye(n)).all():
         return None
     a, b = np.linalg.eig(P.T)
-    l = []
+    res_l = []
     for i in range(len(a)):
         if np.allclose(a[i], 1):
-            l.append(i)
-    if len(l) == 1:
-        return np.abs(b[:, l[0]].T)/np.sum(np.abs(b[:, l[0]].T))
+            res_l.append(i)
+    if len(res_l) == 1:
+        res = (b[:, res_l[0]].T)/np.sum(np.abs(b[:, res_l[0]].T))
+        return np.expand_dims((np.abs(res)), axis=0)
     else:
         return None
