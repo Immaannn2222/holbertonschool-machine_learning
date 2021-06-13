7099c970d7a914609d9120d57e2ea73848535db7
Immaannn2222 <imennaayari@gmail.com>
2021-06-13 20:05:08 +0100

    add the missing 2 tasks

--- supervised_learning/0x10-nlp_metrics/2-cumulative_bleu.py
+++ supervised_learning/0x10-nlp_metrics/2-cumulative_bleu.py
@@ -14,14 +14,14 @@ def NGrams_tokenize(wordlist, n):
 def ngram_bleu(references, sentence, n):
     """calculates the n-gram BLEU score for a sentence"""
     unigrams = len(sentence)
-    token = np.array([len(r) for r in references])
+    token = np.array([len(x) for x in references])
     idx = np.argmin(np.abs(token - unigrams))
-    r = len(references[idx])
-    if r > unigrams:
-        bp = np.exp(1 - r / unigrams)
+    x = len(references[idx])
+    if x > unigrams:
+        bp = np.exp(1 - x / unigrams)
     else:
         return 1
-    references = list(NGrams_tokenize(r, n) for r in references)
+    references = list(NGrams_tokenize(x, n) for x in references)
     sentence = NGrams_tokenize(sentence, n)
     words = {}
     for word in sentence:
@@ -33,3 +33,21 @@ def ngram_bleu(references, sentence, n):
                 words.update({word: ref.count(word)})
     p = (sum(words.values())) / len(sentence)
     return bp * p
+
+
+def cumulative_bleu(references, sentence, n):
+    """Calculates the cumulative n-gram BLEU score for a sentence """
+    sen_size = len(sentence)
+    refs = np.array([len(x) for x in references])
+    min_indx = np.argmin(np.abs(refs - sen_size))
+    x = len(references[min_indx])
+    if x > sen_size:
+        bp = np.exp(1 - x / sen_size)
+    else:
+        return 1
+    ngrams = []
+    for i in range(1, n + 1):
+        ngrams.append(ngram_bleu(references, sentence, i))
+    ngrams = np.array(ngrams)
+    score = np.exp(np.sum((1 / n) * np.log(ngrams)))
+    return bp * score
