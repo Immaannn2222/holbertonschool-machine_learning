566107cd9cf72e510f000ff637ebcdfeb08d42d0
Immaannn2222 <imennaayari@gmail.com>
2021-08-01 22:42:18 +0100

    fix again

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -10,10 +10,10 @@ import datetime
 if __name__ == '__main__':
     url = sys.argv[1]
     response = requests.get(url)
+    if response.status_code == 404:
+        print('Not found')
     if response.status_code == 200:
         print(response.json()['location'])
-    elif response.status_code == 404:
-        print('Not found')
     elif response.status_code == 403:
         header = response.headers['X-Ratelimit-Reset']
         now = datetime.now().timestamp()
