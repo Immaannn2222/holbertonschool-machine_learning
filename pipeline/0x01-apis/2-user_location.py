c8fe08af9dd1d741d41fa2f7827d5a1aa38cd99e
Immaannn2222 <imennaayari@gmail.com>
2021-08-01 22:53:42 +0100

    fix

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -2,21 +2,24 @@
 """
 prints the location of a specific user/ GitHub api
 """
-import requests
 import sys
-import datetime
+import requests
+import time
 
-
-if __name__ == '__main__':
-    """ script that prints the location of a specific user"""
-    url = sys.argv[1]
-    response = requests.get(url)
-    metadata = response.json()
-    if response.status_code == 404:
+if __name__ == "__main__":
+    """
+    If the status code is 403, print Reset in X min
+    where X is the number of minutes from now and
+    the value of X-Ratelimit-Reset
+    """
+    user = sys.argv[1]
+    response = requests.get(user)
+    corpus = response.json()
+    if response .status_code == 404:
         print("Not found")
-    elif response.status_code == 200:
-        print(metadata["location"])
-    elif response.status_code == 403:
+    elif response .status_code == 200:
+        print(corpus["location"])
+    elif response .status_code == 403:
         limit = response.headers["X-Ratelimit-Reset"]
-        timer = (int(limit) - int(datetime.now().timestamp())) / 60
-        print("Reset in {} min".format(int(timer)))
+        x = (int(limit) - int(time.time())) / 60
+        print("Reset in {} min".format(int(x)))
