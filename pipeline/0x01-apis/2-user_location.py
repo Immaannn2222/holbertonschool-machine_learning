9f10905a554a430739fe14827fb681f492c8a82f
Immaannn2222 <imennaayari@gmail.com>
2021-08-01 22:50:51 +0100

    fix the script to get responses headeers

--- pipeline/0x01-apis/2-user_location.py
+++ pipeline/0x01-apis/2-user_location.py
@@ -8,14 +8,15 @@ import datetime
 
 
 if __name__ == '__main__':
+    """ script that prints the location of a specific user"""
     url = sys.argv[1]
-    response = requests.get(url).json()
+    response = requests.get(url)
+    metadata = response.json()
     if response.status_code == 404:
-        print('Not found')
+        print("Not found")
     elif response.status_code == 200:
-        print(response['location'])
+        print(metadata["location"])
     elif response.status_code == 403:
-        header = response.headers['X-Ratelimit-Reset']
-        now = datetime.now().timestamp()
-        time = (int(header) - int(now)) / 60
-        print("Reset in {} min".format(time))
+        limit = response.headers["X-Ratelimit-Reset"]
+        timer = (int(limit) - int(datetime.now().timestamp())) / 60
+        print("Reset in {} min".format(int(timer)))
