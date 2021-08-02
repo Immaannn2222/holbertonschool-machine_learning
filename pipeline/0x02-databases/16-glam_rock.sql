ce2cc9ba77ea7f5afbd65d5468e880d0c829dfa2
Immaannn2222 <imennaayari@gmail.com>
2021-08-02 15:55:14 +0100

    fix checker

--- pipeline/0x02-databases/16-glam_rock.sql
+++ pipeline/0x02-databases/16-glam_rock.sql
@@ -1,4 +1,4 @@
 --  lists all bands with Glam rock as their main style, ranked by their longevity
 SELECT band_name, IF(split IS NULL, 2020 - formed, split - formed) AS lifespan from metal_bands
 WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan;
+ORDER BY lifespan DESC;
