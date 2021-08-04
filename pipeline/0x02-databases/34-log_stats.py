5d3b07787158736a6b01790d2871916ce8e77cc7
Immaannn2222 <imennaayari@gmail.com>
2021-08-04 14:35:28 +0100

    nginx

--- pipeline/0x02-databases/34-log_stats.py
+++ pipeline/0x02-databases/34-log_stats.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
-"""pymongo with nginx"""
+"""pymongo"""
 from pymongo import MongoClient
 
 
 if __name__ == "__main__":
     """provides some stats about Nginx logs stored in MongoDB"""
     client = MongoClient('mongodb://127.0.0.1:27017')
-    doc_count = client.logs.nginx.count_documents({})
+    logs_collection = client.logs.nginx
+    doc_count = logs_collection.count_documents({})
+
     method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    where = {"method": "GET", "path": "/status"}
+    search = {"method": "GET", "path": "/status"}
     print('{} logs'.format(doc_count))
     print('Methods:')
     for i in method:
-        count_method = doc_count.count_documents({"method": i})
+        count_method = logs_collection.count_documents({"method": i})
         print('\tmethod {}: {}'.format(i, count_method))
-    print("{} status check".format(doc_count.count_documents(where)))
+    print("{} status check".format(logs_collection.count_documents(search)))
