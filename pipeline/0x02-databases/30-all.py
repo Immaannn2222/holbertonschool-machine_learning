a24f29f35cfd3c366ee8102bcb7a678d6414b21c
Immaannn2222 <imennaayari@gmail.com>
2021-08-04 11:31:43 +0100

    correct the shebang

--- pipeline/0x02-databases/30-all.py
+++ pipeline/0x02-databases/30-all.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 """pymongo"""
 
 
 def list_all(mongo_collection):
     """ function that lists all documents in a collection """
-    mongo_collection.find()
+    return mongo_collection.find()
